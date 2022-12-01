e220b4bcf7b1ab142ad5880ff4739302338dd40b
kenobyzy <85219738+kenobyzy@users.noreply.github.com>
2022-12-01 10:21:34 +0100

    module 3

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -3,7 +3,7 @@
         <h1>{{ title }}</h1>
         <ul>
             <li v-for="book in books" :key="book.title">
-                {{ book.title }} : {{ book.author }}
+                {{ book.title }}: {{ book.author }}
             </li>
         </ul>
     </div>
@@ -33,10 +33,5 @@ ul {
     list-style-type: none;
     padding: 0;
 
-}
-
-li {
-    display: block;
-    margin: 0 10px;
 }
 </style>
